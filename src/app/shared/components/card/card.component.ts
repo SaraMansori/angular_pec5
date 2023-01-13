a24e5c401ce862c65031f075dba8fe170e3f93f4
Sara Mansori <sara.mansori@cabify.com>
2023-01-13 20:47:42 +0100

    fix and animations added

--- src/app/shared/components/card/card.component.ts
+++ src/app/shared/components/card/card.component.ts
@@ -1,10 +1,37 @@
 import { Component, Input } from '@angular/core';
 import { CharacterDTO } from 'src/app/models/character.dto';
+import {
+  trigger,
+  style,
+  transition,
+  animate,
+  state,
+} from '@angular/animations';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.css'],
+  animations: [
+    trigger('enterState', [
+      state(
+        'void',
+        style({
+          transform: 'translateX(-100%)',
+          opacity: 0,
+        })
+      ),
+      transition(':enter', [
+        animate(
+          300,
+          style({
+            transform: 'translateX(0)',
+            opacity: 1,
+          })
+        ),
+      ]),
+    ]),
+  ],
 })
 export class CardComponent {
   @Input() character!: CharacterDTO;
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,10 +3,16 @@ import { CommonModule } from '@angular/common';
 import { CardComponent } from './components/card/card.component';
 import { MatCardModule } from '@angular/material/card';
 import { AppRoutingModule } from '../app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [CardComponent],
-  imports: [CommonModule, MatCardModule, AppRoutingModule],
+  imports: [
+    CommonModule,
+    MatCardModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+  ],
   exports: [CardComponent],
 })
 export class SharedModule {}
